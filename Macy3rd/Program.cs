a62636a915167a328c5df8e561d852f6e0a6db2b
Macy Draper <Macy>
2019-06-19 12:49:01 -0500

    macy3rd

---
+++ Macy3rd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Macy3rd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

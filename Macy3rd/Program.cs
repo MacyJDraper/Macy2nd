520055032dc6ba7506b742eb9ffc91472fa6fdfb
macyjdraper <macyj93@att.net>
2019-06-20 09:30:20 -0500

    notes from 06/20/19

--- Macy3rd/Program.cs
+++ Macy3rd/Program.cs
@@ -6,8 +6,37 @@ namespace Macy3rd
     {
         static void Main(string[] args)
         {
-            Hello world
-            Console.WriteLine("Hello World!");
+            int numberExample = 0;
+
+            //while
+            //Will only run if true
+            while (numberExample < 10)
+            {
+                Console.WriteLine("hey");
+                numberExample = numberExample + 1;
+                //Other ways to write
+                //numberExample++;
+                //numberExample += 1;
+            }
+            int numberForDoWhileLoop = 0;
+            // do-while
+            // will run once with out checking
+            do
+            {
+                Console.WriteLine("hello");
+            } while (numberForDoWhileLoop < 0);
+
+            // for
+            //Will only run if true
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("yo");
+            }
+
+            
+
+
         }
     }
 }
